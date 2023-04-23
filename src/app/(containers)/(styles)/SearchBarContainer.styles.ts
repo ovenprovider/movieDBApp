f6740bacc0af6e431e746c50a8bc11ade2f45b5d
Ayy <ayy@Ayys-MacBook-Pro.local>
2023-04-23 16:52:54 +1000

    part 2 (final), rename styles file

---
+++ src/app/(containers)/(styles)/SearchBarContainer.styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginTop: 24,
+    marginLeft: 4
+  },
+  searchBarContainer: {
+    flex: 8,
+    flexDirection: 'row'
+  },
+  titleSearchBar: {
+    flex: 2.5
+  },
+  yearSearchBar: {
+    flex: 1,
+    marginLeft: 8
+  },
+  searchIconContainer: {
+    flex: 1,
+    marginLeft: 4,
+    marginRight: 4,
+    alignSelf: 'center'
+  },
+  dropdownContainer: {
+    flex: 1.5,
+    marginLeft: 8
+  }
+})
